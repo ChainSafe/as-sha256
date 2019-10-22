8d82142537e0f8188caa04ee45c82df6db667e20
Marin Petrunić <marin.petrunic@gmail.com>
2019-10-22 10:46:37 +0200

    enable inline wasm code and safe buffer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import wasm from "./wasm";
+import {Buffer} from "safe-buffer";
 
 /**
  * Wraps the AssemblyScript build in a JS function.
@@ -6,7 +7,6 @@ import wasm from "./wasm";
  * @param {Uint8Array} message Message to hash
  */
 export default function sha256(message) {
-  // @ts-ignore
   const buf = Buffer.from(message);
   const arr = wasm.__retain(wasm.__allocArray(wasm.UINT8ARRAY_ID, buf));
   const pointer = wasm.hash(arr);
@@ -17,24 +17,20 @@ export default function sha256(message) {
 }
 
 export function clean() {
-  // @ts-ignore
   wasm.clean();
 }
 
 export function init() {
-  // @ts-ignore
   clean();
 }
 
 export function update(data, length) {
-  // @ts-ignore
   const arr = wasm.__retain(wasm.__allocArray(wasm.UINT8ARRAY_ID, data));
   wasm.update(arr, length);
   wasm.__release(arr);
 }
 
 export function digest() {
-  // @ts-ignore
   const digestPointer = wasm.digest();
   const digest = wasm.__getUint8Array(digestPointer);
   wasm.__release(digestPointer);
--- src/wasm.js
+++ src/wasm.js
@@ -1,6 +1,9 @@
-const fs = require("fs");
-const loader = require("assemblyscript/lib/loader");
-const compiled = new WebAssembly.Module(fs.readFileSync(__dirname + "/../build/optimized.wasm"));
+import wasmCode from "../build/optimized.wasm";
+import {Buffer} from "safe-buffer";
+import * as loader from "assemblyscript/lib/loader";
+
+
+const compiled = new WebAssembly.Module(Buffer.from(wasmCode, 'binary'));
 const imports = {};
 Object.defineProperty(module, "exports", {
     get: () => loader.instantiate(compiled, imports)
